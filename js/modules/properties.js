f8994b6effb70966c4b8e55bca210b85f28736c3
wlimoveis <wlimoveis@zohomail.com>
2026-07-28 11:21:42 -0300

    Update properties.js

--- js/modules/properties.js
+++ js/modules/properties.js
@@ -815,6 +815,9 @@ window.FeatureIconMapper = {
                 // ========== NOVAS ENTRADAS PARA ORLA E ACESSO RÁPIDO ==========
         { keywords: ['orla', 'avenida', 'pajuçara', 'ponta verde', 'beira mar', 'beira-mar', 'litoral', 'praia'], icon: 'fa-umbrella-beach', color: '#f39c12', label: 'Orla/Praia' },
         { keywords: ['acesso rápido', 'acesso', 'rápido', 'proximidade orla', 'perto orla', 'acesso fácil'], icon: 'fa-bolt', color: '#e67e22', label: 'Acesso Rápido' },
+                // ========== NOVAS ENTRADAS PARA PAVIMENTOS E ANDARES ==========
+        { keywords: ['pavimento', 'pavimentos', 'andar', 'andares', 'piso', 'pisos', 'sobreloja'], icon: 'fa-layer-group', color: '#6c5ce7', label: 'Pavimentos' },
+        { keywords: ['térreo', 'terreo', 'r/c', 'rc', 'res-do-chão', 'res do chão'], icon: 'fa-ground', color: '#00b894', label: 'Térreo' },
         { keywords: ['quarto', 'dormitório', 'dormitorio', 'suíte', 'suite', 'quartos', 'qtos', 'qto', 'qts'], icon: 'fa-bed', color: '#e74c3c', label: 'Quarto' },
         { keywords: ['banheiro', 'wc', 'lavabo', 'banheiros'], icon: 'fa-shower', color: '#1abc9c', label: 'Banheiro' },
         { keywords: ['cozinha', 'copa', 'cozinha americana'], icon: 'fa-utensils', color: '#f39c12', label: 'Cozinha' },
