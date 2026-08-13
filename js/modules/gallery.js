8039a605215e7e52b48e1b063119bc3c53866cdf
wlimoveis <wlimoveis@zohomail.com>
2026-08-13 18:23:50 -0300

    Update gallery.js

--- js/modules/gallery.js
+++ js/modules/gallery.js
@@ -247,7 +247,7 @@ function renderDiagonalBadges(property) {
     if (badges.length === 0) return '';
 
     // 🔴 CORRIGIDO: Aumentar espaçamento para evitar sobreposição
-    var baseTop = 18;
+    var baseTop = 25;
     var spacing = 52;  // 🔴 52px (antes 42px) - sem sobreposição
 
     var result = '';
