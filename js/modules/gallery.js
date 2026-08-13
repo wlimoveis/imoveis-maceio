eb93820bf1c29f92214d515459960c0977898662
wlimoveis <wlimoveis@zohomail.com>
2026-08-13 11:34:43 -0300

    Update gallery.js

--- js/modules/gallery.js
+++ js/modules/gallery.js
@@ -262,7 +262,7 @@ function renderDiagonalBadges(property) {
 
         if (isFirst) {
             fontSize = '0.9rem';
-            padding = '7px 150px 7px 45px';  // Em vez de '7px 45px'
+            padding = '7px 140px 7px 45px';  // Em vez de '7px 45px'
             width = '300px';        // 🔴 Aumentado
             leftOffset = '-40px';
             letterSpacing = '3px';
@@ -882,5 +882,5 @@ if (document.readyState === 'loading') {
     window.setupGalleryEvents();
 }
 
-// ======== EXPOSIÇÃO DA FUNÇÃO openGallery COMO ALIAS =====
+// ======= EXPOSIÇÃO DA FUNÇÃO openGallery COMO ALIAS =====
 window.openGallery = window.openGalleryAtCurrentIndex;
