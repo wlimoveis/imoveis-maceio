e67f6a90809d5fc52078c065d733df339bb11aa9
wlimoveis <wlimoveis@zohomail.com>
2026-08-14 12:58:06 -0300

    Update gallery.js

--- js/modules/gallery.js
+++ js/modules/gallery.js
@@ -879,5 +879,5 @@ if (document.readyState === 'loading') {
     window.setupGalleryEvents();
 }
 
-// ========= EXPOSIÇÃO DA FUNÇÃO openGallery COMO ALIAS =========
+// ========= EXPOSIÇÃO DA FUNÇÃO openGallery COMO ALIAS ========
 window.openGallery = window.openGalleryAtCurrentIndex;
