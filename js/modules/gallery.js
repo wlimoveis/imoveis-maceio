2aa804cd6d2b510c48ef710aa81ca9bb4ea9585b
wlimoveis <wlimoveis@zohomail.com>
2026-08-19 07:23:43 -0300

    Update gallery.js

--- js/modules/gallery.js
+++ js/modules/gallery.js
@@ -279,7 +279,7 @@ function renderDiagonalBadges(property) {
     var badges = generateDiagonalBadges(property);
     if (badges.length === 0) return '';
 
-    var baseTop = 7;
+    var baseTop = 6;
     var spacing = 52;
 
     var result = '';
