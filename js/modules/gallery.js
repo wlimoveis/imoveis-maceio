0b695c4fc5a454f75c559a4b84d4099771cceb17
wlimoveis <wlimoveis@zohomail.com>
2026-08-19 18:38:28 -0300

    Update gallery.js

--- js/modules/gallery.js
+++ js/modules/gallery.js
@@ -317,7 +317,7 @@ function renderDiagonalBadges(property) {
         } else {
             // 🔴 DESTAQUE 2
             fontSize = '0.65rem';
-            padding = '10px 30px 1px 30px';
+            padding = '10px 25px 1px 25px';
             width = '220px';
             leftOffset = '-32px';
             letterSpacing = '1.5px';
