70cca5911e0c9aa0c4a05d15d35660d14457bbd7
wlimoveis <wlimoveis@zohomail.com>
2026-08-23 07:37:19 -0300

    Update gallery.js

--- js/modules/gallery.js
+++ js/modules/gallery.js
@@ -456,7 +456,7 @@ function generateBookmarkBadge(property) {
         fontSize = '0.45rem';
         padding = '6px 6px 6px 6px';
         width = '182px';          // 🔴 ESTICADO (cobre a largura da foto)
-        height = '45px';
+        height = '25px';
         topOffset = '50px';
         rightValue = '150px';      // 🔴 ENCOSTA NA BORDA DIREITA
         iconSize = '0.5rem';
@@ -464,7 +464,7 @@ function generateBookmarkBadge(property) {
         fontSize = '0.5rem';
         padding = '8px 8px 8px 8px';
         width = '192px';          // 🔴 ESTICADO (cobre a largura da foto)
-        height = '55px';
+        height = '30px';
         topOffset = '50px';
         rightValue = '150px';     // 🔴 ENCOSTA NA BORDA DIREITA
         iconSize = '0.6rem';
