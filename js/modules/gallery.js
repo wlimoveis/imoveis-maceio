d5c9540c3a500c02b8888b5e4cc4b6a348b1adb9
wlimoveis <wlimoveis@zohomail.com>
2026-08-24 15:36:30 -0300

    Update gallery.js

--- js/modules/gallery.js
+++ js/modules/gallery.js
@@ -448,7 +448,7 @@ function generateBookmarkBadge(property) {
         displayText = 'DESÁGIO';
     }
 
-    var baseTop = isMobile ? 10 : 6;
+    var baseTop = isMobile ? 8 : 5;
     var fontSize, padding, width, leftOffset, letterSpacing, topOffset;
     var textAlign;
 
